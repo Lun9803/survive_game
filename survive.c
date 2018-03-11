add1c7773c89b1b7aa809fe07088f935b863f83c
bolun98 <bolunlun9805@163.com>
2018-03-11 17:41:49 +1100

    basic frame

---
+++ survive.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "survive_header.h"
+
+
+int main(){
+    printf("game starts now, please enter your charactor name\n");
+    scanf("%s", name);
+    printline();
+    printf("Hello, %s! Welcome to the dark world, in this world, you need to try your best to survive.\n", &name);
+    printf("Here is your attributes: health: %d, hunger: %d, energy: %d, armor: %d, attack: %d\n", health, hunger, energy, armor, attack);
+
+    char command[20];
+    printf("Type 'go' to start, or 'q' to quit\n");
+    scanf("%s", command);
+    while(strcmp(command, "q") != 0){
+        printline();
+        printf("Day %d begins, the time is %d:00\n", day, hour);
+        printline();
+
+        int move;
+        while(hour < 24){
+            printf("you have the following options:\n   1. Find food ()\n   2. Hunt ()\n   3. Rest ()\n");
+            printf("Enter the number of the move\n");
+            scanf("%d", &move);
+            if(move == 1){
+                if(24-hour >= findfoodTime){
+                    findfood();
+                }
+                else{
+                    timewarn();
+                }
+            }
+
+            else if(move == 2){
+                if(24-hour >= huntTime){
+                    hunt();
+                }
+                else{
+                    timewarn();
+                }
+            }
+
+            else if(move == 3){
+                if(24-hour >= restTime){
+                    rest();
+                }
+                else{
+                    timewarn();
+                }
+            }
+
+            printf("The time is %d:00\n", hour);
+        }
+
+        printf("Day %d has ended, congrats! type 'q' to quit or 'go' to continue\n", day);
+        printline();
+        day++;
+        sleep();
+        scanf("%s", command);
+    }
+}
+
+
--- survive_header.h
+++ survive_header.h
@@ -1,6 +1,58 @@
 #ifndef SURVIVE_HEADER_H_INCLUDED
 #define SURVIVE_HEADER_H_INCLUDED
 
+#include <stdio.h>
+int health = 100;
+int hunger = 100;
+int energy = 100;
+int armor = 0;
+int attack = 10;
+char name[20];
+
+int findfoodTime = 1;
+int huntTime = 3;
+int restTime = 1;
+
+int day = 1;
+int hour = 8;
+
+typedef struct wolf {
+	int attack: 30;
+	int armor: 10;
+	int health: 30;
+} wolf;
+
+
+void printline(){
+    printf("=======================================================\n");
+}
+
+void timewarn(){
+    printf("You don't have enough time to do that\n");
+}
+
+void findfood(){
+    hour+=findfoodTime;
+}
+
+void hunt(){
+    hour+=huntTime;
+}
+
+void rest(){
+    hour+=restTime;
+    energy+=10;
+    if(energy > 100){
+        energy = 100;
+    }
+    printf("Your energy increases to %d\n", energy);
+}
+
+void sleep(){
+    hunger -= 60;
+    energy = 100;
+    hour = 8;
+}
 
 
 #endif // SURVIVE_HEADER_H_INCLUDED
